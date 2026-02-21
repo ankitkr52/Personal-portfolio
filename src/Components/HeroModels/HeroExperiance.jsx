ba5ef3d70609d9f65e3705130a60bc1e613f02fe
ankitkr52 <ankit72p@gmail.com>
2026-02-21 23:30:28 +0530

    fixed 3d model

--- src/Components/HeroModels/HeroExperiance.jsx
+++ src/Components/HeroModels/HeroExperiance.jsx
@@ -2,14 +2,16 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import { useMediaQuery } from 'react-responsive';
+import { Room } from './Room';
+import HeroLights from './HeroLights';
+import Particles from './Particles';
 
 const HeroExperiance = () => {
     const isTablet = useMediaQuery({ query: '(max-width:1024px)' });
     const isMobile = useMediaQuery({ query: '(max-width:768px)' });
     return (
         <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-            <ambientLight intensity={0.2} color="#1a1a40" />
-            <directionalLight position={[5, 5, 5]} intensity={5} />
+
             <OrbitControls
                 enablePan={false}
                 enableZoom={!isTablet}
@@ -18,12 +20,16 @@ const HeroExperiance = () => {
                 minPolarAngle={Math.PI / 5}
                 maxPolarAngle={Math.PI / 2}
             />
+            <HeroLights />
+            <Particles count={100}/>
+            <group
+                scale={isMobile ? 0.7 : 1}
+                position={[0, -3.5, 0]}
+                rotation={[0, -Math.PI / 4, 0]}
+            >
 
-            <mesh>
-                <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color="teal" />
-            </mesh>
-
+            </group>
+            <Room />
         </Canvas>
     )
 }
