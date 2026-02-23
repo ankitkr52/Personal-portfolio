949407e370bc5cfce38277e7cc6be60e0f20601e
ankitkr52 <ankit72p@gmail.com>
2026-02-23 10:10:40 +0530

    fixed navbar and project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Hero from './Sections/Hero'
+import ShowcaseSection from './Sections/ShowcaseSection'
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
     <>
-    <Hero/>
-    {/*  */}
+    <Navbar/>
+      <Hero />
+      <ShowcaseSection />
     </>
   )
 }
